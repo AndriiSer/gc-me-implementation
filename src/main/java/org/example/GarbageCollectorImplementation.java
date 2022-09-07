14c3407d93a00317213cfbbeb41f500b4ede5e58
AndriiSer <108199904+AndriiSer@users.noreply.github.com>
2022-09-07 03:49:51 +0300

    1st test

---
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -0,0 +1,39 @@
+package org.example;
+
+
+import java.util.*;
+
+
+public class GarbageCollectorImplementation implements GarbageCollector {
+  @Override
+  public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
+    Map<String, ApplicationBean> beans = heap.getBeans();
+    Deque<StackInfo.Frame> frames = stack.getStack();
+    List<ApplicationBean> deadBeans = new ArrayList<>(beans.values());
+    Set<ApplicationBean> aliveBeans = getAliveBeans(frames);
+    deadBeans.removeAll(aliveBeans);
+    return deadBeans;
+  }
+
+  private Set<ApplicationBean> getAliveBeans(Deque<StackInfo.Frame> frames) {
+    Set<ApplicationBean> aliveBeans = new HashSet<>();
+    for (StackInfo.Frame frame : frames) {
+      for (ApplicationBean bean : frame.parameters) {
+        aliveBeans.addAll(getRelations(bean, aliveBeans));
+      }
+    }
+    return aliveBeans;
+  }
+
+
+  private Set<ApplicationBean> getRelations(ApplicationBean bean, Set<ApplicationBean> aliveSet) {
+    if (!aliveSet.contains(bean)) {
+      aliveSet.add(bean);
+      for(Map.Entry<String, ApplicationBean> beanEntry : bean.getFieldValues().entrySet()) {
+        getRelations(beanEntry.getValue(), aliveSet);
+      }
+    }
+    return aliveSet;
+  }
+}
+
